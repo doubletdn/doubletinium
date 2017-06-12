936c08c33dec66c3602978713e9d33b5312f244a
doubletdn <hdkani@gmail.com>
2017-06-12 23:33:23 +0700

    add void JsClickOn()

--- doubletinium/Common/Actions.cs
+++ doubletinium/Common/Actions.cs
@@ -13,6 +13,7 @@ namespace doubletinium.Common
     public static class Action
     {
         public static void JsClickOn(this IWebElement element)
+            //action click on element using javascript
         {
             string js = "arguments[0].click();";
             ((IJavaScriptExecutor)BrowserFactory.Driver).ExecuteScript(js, element);
