fd5fa360e8d8c3ce869d92a8bca491f283e27f32
DoubleT <hdkani@gmail.com>
2017-06-12 22:06:25 +0700

    add missing class

---
+++ doubletinium/PageObjects/Joomla/UsersPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using doubletinium.Common;
+
+namespace doubletinium.PageObjects.Joomla
+{
+    public class UsersPage
+    {
+
+        [FindsBy(How = How.XPath, Using = "//input[@type='checkbox' and @name='checkall-toggle']")]
+        private IWebElement CheckAllToggle { get; set; }
+
+        public void SelectAllRecords()
+        {
+            CheckAllToggle.Set("on");
+        }
+    }
+}
